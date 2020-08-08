b8030d2518c3f753766a6542fc2b8b0ac7308989
MarkMburu <markmburu685@gmail.com>
2020-08-08 20:34:38 +0300

    initial-commit

---
+++ src/components/books.jsx
@@ -0,0 +1,35 @@
+//-*- mode: rjsx-mode;
+
+import React from 'react';
+
+export default class Books extends React.Component {
+
+  constructor(props) {
+    super(props);
+    //@TODO other initialization
+    
+  }
+
+  //@TODO other methods
+
+  render() {
+    //@TODO complete rendering
+    return '';
+  }
+
+}
+
+export function Book({book, full}) {
+  //@TODO return rendering of book based on full
+  console.log('booook....', book)
+  console.log('', full)
+  return '';
+}
+
+//@TODO other components like SearchForm, Results, etc.
+
+/** text for scroll controls */
+const SCROLLS = {
+  next: 'Next >>',
+  prev: '<< Previous',
+};
